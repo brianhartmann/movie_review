e9757ea3729a30dbf94f496858c14b52fbe3a45b
brianhartmann <bjhartmann13@gmail.com>
2025-04-17 17:40:09 -0600

    add / mapping

--- src/main/java/com/example/movie_review/MovieReviewApplication.java
+++ src/main/java/com/example/movie_review/MovieReviewApplication.java
@@ -2,7 +2,10 @@ package com.example.movie_review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class MovieReviewApplication {
 
@@ -10,4 +13,8 @@ public class MovieReviewApplication {
         SpringApplication.run(MovieReviewApplication.class, args);
     }
 
+    @GetMapping("/")
+    public String index() {
+        return "Hello World";
+    }
 }
